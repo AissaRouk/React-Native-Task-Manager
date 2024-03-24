ed42d6900786b2a17214ed046d8680325dedbe05
AissaRouk <aissa.rouk@gmail.com>
2024-03-24 03:45:00 +0100

    enums added2

--- src/Types/Types.ts
+++ src/Types/Types.ts
@@ -8,14 +8,40 @@ export interface Task {
 
 export interface Day {
   id: string;
-  dayOfTheWeek: "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun";
+  dayOfTheWeek: dayNames;
   day: number;
   tasks: Task[] | [];
 }
 
 export interface Month {
   id: string;
-  name: string; // Name of the month (e.g., "January", "February", etc.)
+  name: monthNamesEnum | string; // Name of the month (e.g., "January", "February", etc.)
   year: number; // Year of the month
   days: Day[]; // Array of Day objects representing the days in the month
+  totalDays: number;
+}
+
+export enum dayNames {
+  MONDAY = "Mon",
+  TUESDAY = "Tue",
+  WEDNESDAY = "Wed",
+  THURSDAY = "Thu",
+  FRIDAY = "Fri",
+  SATURDAY = "Sat",
+  SUNDAY = "Sun",
+}
+
+export enum monthNamesEnum {
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 }
