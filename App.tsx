816d0c96f199fda39238f90d85f38c57f6cf51a4
AissaRouk <aissa.rouk@gmail.com>
2024-05-08 13:27:27 +0300

    Fonts added

--- App.tsx
+++ App.tsx
@@ -1,24 +1,11 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React from 'react';
 import {StyleSheet, SafeAreaView, StatusBar} from 'react-native';
-import {useFonts} from 'expo-font';
 import TasksViewComponent from './src/Components/TasksViewComponent';
 import ContextProvider from './src/Context/Context';
 import HeaderComponent from './src/Components/HeaderComponent';
 import CalendarScrollViewComponent from './src/Components/CalendarScrollView';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Gilroy-Black': require('./assets/Fonts/Gilroy-Black.ttf'),
-    'Gilroy-Regular': require('./assets/Fonts/Gilroy-Regular.ttf'),
-    'Gilroy-Medium': require('./assets/Fonts/Gilroy-Medium.ttf'),
-    'Gilroy-SemiBold': require('./assets/Fonts/Gilroy-SemiBold.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    // Handle font loading state
-    return null;
-  }
-
   return (
     <ContextProvider>
       <SafeAreaView style={styles.container}>
