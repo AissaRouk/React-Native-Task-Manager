4e67682d6d198be607b37b9c10de64e86de3236f
AissaRouk <aissa.rouk@gmail.com>
2024-05-26 16:30:12 +0300

    code improved

--- src/Components/HeaderComponent.tsx
+++ src/Components/HeaderComponent.tsx
@@ -1,10 +1,10 @@
-import { useContext } from "react";
-import { StyleSheet, View, Text } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-import { AppContext } from "../Context/Context";
+import {useContext} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/Ionicons';
+import {AppContext} from '../Context/Context';
 
 export default function HeaderComponent() {
-  const { currentMonth } = useContext(AppContext);
+  const {currentMonth} = useContext(AppContext);
 
   return (
     <View style={styles.header}>
@@ -19,23 +19,23 @@ export default function HeaderComponent() {
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "100%",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
     paddingHorizontal: 20,
     paddingTop: 10,
     paddingBottom: 20,
   },
   headerTitle: {
-    fontFamily: "Gilroy-SemiBold",
+    fontFamily: 'Gilroy-SemiBold',
     fontSize: 22,
-    color: "white",
+    color: 'white',
   },
   headerIcons: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   icon: {
     marginHorizontal: 10,
-    color: "white",
+    color: 'white',
   },
 });
