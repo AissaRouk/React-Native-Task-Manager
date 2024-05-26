b35a6d554f7ae3ff23a4d6ed24f1374b3813169e
AissaRouk <aissa.rouk@gmail.com>
2024-05-26 16:28:03 +0300

    added channel creation for PushNotifiaction and requestNotification

--- App.tsx
+++ App.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
-import {StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  PermissionsAndroid,
+} from 'react-native';
 import TasksViewComponent from './src/Components/TasksViewComponent';
 import ContextProvider from './src/Context/Context';
 import HeaderComponent from './src/Components/HeaderComponent';
 import CalendarScrollViewComponent from './src/Components/CalendarScrollView';
+import PushNotification from 'react-native-push-notification';
 
-export default function App() {
+const requestNotificationPermission = async () => {
+  const exists = await PermissionsAndroid.check(
+    PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+  );
+  if (!exists) {
+    const isGranted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      {
+        title: 'Task Manager App Camera Permission',
+        message: 'Allow this permission so the app can work',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (isGranted == PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('Notification permission granted ');
+    } else console.error('Notification permission not granted!!');
+  }
+};
+
+const App = () => {
+  // Function to schedule a local notification (modify notification details as needed)
+  requestNotificationPermission();
+  // Handle notification channel creation on component mount (Android Oreo+)
+  useEffect(() => {
+    //Request permission for notifications
+
+    //create a newChanner if it doesn't exist already
+    PushNotification.createChannel(
+      {
+        channelId: 'notification',
+        channelName: 'My Notification Channel',
+        channelDescription: 'A channel for scheduled notifications',
+        importance: 4, // Importance level (HIGH in this case),
+      },
+      created => {
+        console.log('Notification channel: ' + created);
+      },
+    );
+
+    // Schedule the notification after creating the channel
+    // scheduleLocalNotification();
+  }, []);
+
   return (
     <ContextProvider>
       <SafeAreaView style={styles.container}>
@@ -18,7 +67,7 @@ export default function App() {
       </SafeAreaView>
     </ContextProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -27,3 +76,5 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight,
   },
 });
+
+export default App;
