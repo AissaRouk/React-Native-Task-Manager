1e1553e02220511f41866a7723fe20ab3e7cb94d
AissaRouk <aissa.rouk@gmail.com>
2024-06-04 11:04:58 +0300

    Code optimized

--- src/Components/TaskComponent.tsx
+++ src/Components/TaskComponent.tsx
@@ -1,33 +1,31 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import getRandomColor from "../Utils/getRandomColor";
-import { Task } from "../Types/Types";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import getRandomColor from '../Utils/getRandomColor';
+import {Task} from '../Types/Types';
 
-export default function TaskComponent({ task }: { task: Task }) {
+export default function TaskComponent({task}: {task: Task}) {
   const backgroundColor = getRandomColor();
-
-  console.log("TaskComponent -> tasl:" + JSON.stringify(task));
 
   // Function to format time as hh:mm
   const formatTime = (date: Date) => {
-    console.log("Date: " + date + " type: " + typeof date);
+    console.log('Date: ' + date + ' type: ' + typeof date);
     if (!date || !(date instanceof Date)) {
       return new Date(date).toLocaleTimeString();
     }
 
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
     return `${hours}:${minutes}`;
   };
 
   return (
     <View style={styles.container}>
-      <View style={[styles.taskView, { backgroundColor }]}>
+      <View style={[styles.taskView, {backgroundColor}]}>
         <Text style={styles.title}>{task.name}</Text>
         <Text style={styles.content}>{task.content}</Text>
       </View>
       <View style={styles.timeView}>
-        <View style={[styles.dot, { backgroundColor }]} />
+        <View style={[styles.dot, {backgroundColor}]} />
         <Text style={styles.timeText}>{formatTime(task.date)}</Text>
       </View>
     </View>
@@ -38,32 +36,32 @@ const styles = StyleSheet.create({
   container: {
     borderRadius: 20,
     marginBottom: 20,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   taskView: {
     padding: 15,
     borderRadius: 20,
-    maxWidth: "75%",
+    maxWidth: '75%',
   },
   title: {
-    fontFamily: "Gilroy-SemiBold",
+    fontFamily: 'Gilroy-SemiBold',
     fontSize: 19,
   },
   content: {
     marginTop: 5,
-    fontFamily: "Gilroy-Regular",
-    overflow: "hidden",
+    fontFamily: 'Gilroy-Regular',
+    overflow: 'hidden',
   },
   time: {
-    fontFamily: "Gilroy-SemiBold",
+    fontFamily: 'Gilroy-SemiBold',
     fontSize: 15,
     marginTop: 10,
   },
   timeView: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   dot: {
     width: 10,
@@ -72,7 +70,7 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   timeText: {
-    fontFamily: "Gilroy-SemiBold",
+    fontFamily: 'Gilroy-SemiBold',
     fontSize: 15,
   },
 });
