35919b67b52cc94c64afbe7c0296b5b9716f9cc9
AissaRouk <aissa.rouk@gmail.com>
2024-06-06 09:43:56 +0300

    added Dropdown without functionality

--- src/Components/CalendarScrollView.tsx
+++ src/Components/CalendarScrollView.tsx
@@ -1,19 +1,26 @@
-import React, { useContext, useEffect, useRef } from "react";
-import { ScrollView, StyleSheet } from "react-native";
-import DayComponent from "./DayComponent";
-import { AppContext } from "../Context/Context";
-import getDayOfWeek from "../Utils/getDayOfWeek";
+import React, {useContext, useEffect, useRef} from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
+import DayComponent from './DayComponent';
+import {AppContext} from '../Context/Context';
+import getDayOfWeek from '../Utils/getDayOfWeek';
 
 export default function CalendarScrollViewComponent() {
-  const { currentMonth, currentDay } = useContext(AppContext);
+  const {currentMonth, currentDay} = useContext(AppContext);
   const scrollViewRef = useRef<ScrollView>(null);
 
   useEffect(() => {
     // Scroll to today's date when the component mounts
+    console.log(
+      'CalendarScrollViewComponent useEffect.currentMonth -> ' +
+        JSON.stringify(currentMonth) +
+        '\ncurrentDay: ' +
+        JSON.stringify(currentDay),
+    );
     if (scrollViewRef.current && currentMonth && currentDay) {
+      console.log('entered');
       if (currentDay && currentDay.day > 1) {
         const offset = (currentDay.day - 1) * (55 + 16); // Adjust based on your DayComponent's width and margin
-        scrollViewRef.current.scrollTo({ x: offset, animated: true });
+        scrollViewRef.current.scrollTo({x: offset, animated: true});
       }
     }
   }, [currentMonth, currentDay]);
@@ -22,26 +29,25 @@ export default function CalendarScrollViewComponent() {
     return null; // Return null if currentMonth is not available
   }
 
-  const daysInMonth = currentMonth.totalDays;
-
   return (
     <ScrollView
       ref={scrollViewRef}
       horizontal={true}
       style={styles.scrollView}
-      showsHorizontalScrollIndicator={false}
-    >
-      {[...Array(daysInMonth)].map((_, index) => {
+      showsHorizontalScrollIndicator={false}>
+      {/* I'm creating an array in here to make a Daycomponent for each day that exists in the month */}
+      {[...Array(currentMonth.totalDays)].map((_, index) => {
         // Calculate the day number
         const dayNumber = index + 1;
-        const day = currentMonth.days?.find((d) => d.day === dayNumber) || {
+        // If the day exists in the month, fetch it, if not create an empty new day
+        const day = currentMonth.days?.find(d => d.day === dayNumber) || {
           day: dayNumber,
           dayOfTheWeek: getDayOfWeek(
             currentMonth.year,
             currentMonth.name,
-            dayNumber
+            dayNumber,
           ),
-          id: dayNumber + ":" + currentMonth.name,
+          id: dayNumber + ':' + currentMonth.name,
           tasks: [],
         };
 
