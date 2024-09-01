3cdc5891f97dbd1f26767f3fed4eb3d774f45f4c
AissaRouk <aissa.rouk@gmail.com>
2024-09-01 15:34:33 +0300

    fixed problem

--- src/Utils/getMonthIndex.tsx
+++ src/Utils/getMonthIndex.tsx
@@ -1,10 +1,13 @@
-import {Month} from '../Types/Types';
+import {Month, monthNamesEnum} from '../Types/Types';
 
 export default function getMonthIndex(month: Month): number {
-  // Assuming month.name is a valid month name string
-  const monthNumber = new Date(month.year, month.name).getMonth();
-  if (monthNumber === -1) {
-    throw new Error('Invalid month name in Month object');
-  }
-  return monthNumber;
+  // Get the name of the month from the Month object
+  const monthName: monthNamesEnum = month.name;
+
+  // console.log(
+  //   'getMonthIndex -> month.name: ' + typeof monthNamesEnum[monthName],
+  // );
+
+  // console.log('getMonthIndex -> return: ' + monthNamesEnum[monthName]);
+  return Number(monthNamesEnum[monthName]);
 }
